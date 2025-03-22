d58864be19501b0e0de8d05298d18ec340747e64
Leonardo <ldrigues.dev@gmail.com>
2025-03-22 13:54:51 -0300

    feat: add jwt stratedy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { PrismaService } from "./prisma/prisma.service";
 import { Module } from "@nestjs/common";
 
 import { AuthModule } from "./auth/auth.module";
-
-import { AuthenticateController } from "./controllers/authenticate-controller";
-import { CreateAccountController } from "./controllers/create-account.controller";
-
 import { envSchema } from "./env";
+
+import { AuthenticateController } from "./controllers/authenticate.controller";
+import { CreateAccountController } from "./controllers/create-account.controller";
+import { CreateQuestionController } from "./controllers/create-question.controller";
+
 
 @Module({
   imports: [
@@ -17,7 +18,11 @@ import { envSchema } from "./env";
     }),
     AuthModule,
   ],
-  controllers: [CreateAccountController, AuthenticateController],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
